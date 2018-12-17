a65453b77c60f6d8d0b07643c5b712754b7d3c69
Stepan Karpenko <stepan.karpenko@gmail.com>
2018-12-16 16:06:22 -0800

    Suppressed Verilator WIDTH warnings.

--- rtl/src/memory.v
+++ rtl/src/memory.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2017 The Ultiparc Project. All rights reserved.
+ * Copyright (c) 2015-2018 The Ultiparc Project. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -105,6 +105,7 @@ begin
 	end
 	else
 	begin
+	/* verilator lint_off WIDTH */
 		case(i_MCmd)
 		`OCP_CMD_WRITE: begin
 			if(i_MAddr[`ADDR_WIDTH-1:2] < MEMWORDS)
@@ -130,6 +131,7 @@ begin
 			o_SResp <= `OCP_RESP_NULL;
 		end
 		endcase
+	/* verilator lint_on WIDTH */
 	end
 end
 
